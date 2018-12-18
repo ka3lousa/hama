9b8656f785c9140e1754a83ee938235c453ea4db
ka3lousa <45945493+ka3lousa@users.noreply.github.com>
2018-12-18 20:41:24 +0100

    hamaed

--- bot.js
+++ bot.js
@@ -4,9 +4,10 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("523808032921092116")
+let channel =     client.channels.get("524671976799862794")
 setInterval(function() {
-channel.send(`hi  i am the best in this houma i am boyka kabir enta kabir haywan kalb`);
+channel.send(`hi  i am the best in this houma i am boyka kabir enta kabir haywan kalbhi hi hi hi hih i hih ih ihih i hih ih ih ihi h ihih ih ih ihihihi hi ihihihihihihihihihihihihihi salut 
+sava savavavavavav go to room to goteher plzt tjkqftsk qfjkqsnfos iosjfisjfsifaiiaj aizeariazearetertreyytruqfq`);
 }, 30)
 })
 
